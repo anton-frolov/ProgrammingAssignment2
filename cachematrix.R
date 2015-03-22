8ad710e978e17e76cd8c19a0907ec345d5d6953f
anton_frolov <antonfv@gmail.com>
2015-03-22 14:30:35 +0500

    PA 2 функции для кэширования обратных матриц

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,57 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## initialize properties
+  inv <- NULL
+  
+  ## set and get of matrix 
+  set <- function(matix) {
+    x <<- matrix
+    inv <<- NULL
+  }
+  get <- function(){ 
+    x 
+  }
+  
+  ## set and get inverse of matrix
+  setInverse <- function(inverse){
+    inv <<- inverse
+  }
+  getInverse <- function(){
+    inv
+  }
+  
+  ## list of the methods
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  ## check into cache 
+  m <- x$getInverse()
+  if(!is.null(m)){
+    message("getting cached data")
+    ## if there is, then return it
+    return(m)
+  }
+  ## if "m" is NULL, get original matrix
+  data <- x$get()
+  ## calculate inverse  
+  m <- solve(data, ...)
+   
+  ## set inverse to cache
+  x$setInverse(m)
+  
+  ## return result
+  m
 }
